d40907a9b0fae7bdade9a52fc13fdc9ad3069432
Ben <Bendavcorr@Gmail.com>
2023-12-14 14:28:01 +0000

    Update main.py

--- main.py
+++ main.py
@@ -25,15 +25,11 @@ def test():
     #return "Test" # testing 
     return(get_api_key())
 
-@app.route("/add",methods=['GET','POST'])
+@app.route("/add",methods=['POST'])
 def add():
-  if request.method=='GET':
-    return "Use post to add" # replace with form template
-  else:
     token=get_api_key()
     ret = addWorker(token,request.form['num'])
     return ret
-
 
 def addWorker(token, num):
     with open('payload.json') as p:
